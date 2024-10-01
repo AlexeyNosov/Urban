2c50651888cf38d97e59acb1b45ce31d00fb5bb4
AlexeyNosov <tofreebike@gmail.com>
2024-10-02 00:00:44 +0300

    Add files via upload

--- module3_4.py
+++ module3_4.py
@@ -1,9 +1,9 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if i in root_word or root_word in i:
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
             same_words.append(i)
     return same_words
-print(single_root_words("bol", "footbol", "bol", "bolbol", "bal", "bolobol", "lob", "ol", "b"))
+print(single_root_words("Bol", "footboL", "bOL", "BOLBOL", "bal", "bolobol", "lob", "ol", "b"))
 
 
